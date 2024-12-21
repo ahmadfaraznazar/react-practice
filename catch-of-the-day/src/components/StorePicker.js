01d6454da3ddae376f89358b4122320231f5a038
Ahmad Nazar <ahmadfaraznazar@gmail.com>
2024-12-21 15:26:22 +0500

    created first react component StorePciker

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Component } from "react";
+
+
+export default class StorePicker extends Component{
+    render(){
+        return (
+            <p>I'm a StorePicker</p>
+        )
+    }
+} 
